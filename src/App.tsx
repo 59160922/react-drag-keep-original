4f75d3c529e4295208d7c997310f671619022570
ittiopon kewngam <59160922@go.buu.ac.th>
2021-11-06 17:15:07 +0700

    'ryou'

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState,useEffect } from "react";
+import "./App.css";
+
 
 function App() {
+  const [displaySection, setDisplaySection] = useState<section[]>([])
+
+  useEffect(() => {
+    console.log(displaySection);
+    
+  }, [displaySection])
+
+  enum section {
+    pattern1 ='pattern1',
+    pattern2 ='pattern2',
+    pattern3 ='pattern3',
+  }
+
+  function dragStart(event: any) {
+    event.dataTransfer.setData("Text", event.target.id);
+
+  
+  }
+
+  function allowDrop(event: any) {
+    event.preventDefault();
+  }
+
+  function drop(ev: any) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("Text");
+    console.log(data);
+    
+    switch(data){
+      case (section.pattern1):
+       setDisplaySection([...displaySection,section.pattern1])
+        break;
+      case (section.pattern2):
+        setDisplaySection([...displaySection,section.pattern2])
+        break;  
+      case (section.pattern3):
+        setDisplaySection([...displaySection,section.pattern3])
+        break;  
+    }
+    
+     ev.target.appendChild(document.getElementById(data)?.cloneNode(true));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div>
+      <div
+        className="droptarget"
+        onDrop={drop}
+        onDragOver={allowDrop}
+     
+      >
+        <p onDragStart={dragStart} draggable="true" id="pattern1">
+          Pattern1
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p onDragStart={dragStart} draggable="true" id="pattern2">
+          Pattern2
+        </p>
+        <p onDragStart={dragStart} draggable="true" id="pattern3">
+          Pattern3
+        </p>
+      </div>
+
+      <div
+        className="droptarget drop-receive"
+        onDrop={drop}
+        onDragOver={allowDrop}
+      ></div>
     </div>
   );
 }
